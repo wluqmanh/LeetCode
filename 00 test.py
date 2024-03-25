96ec1d1a39e0c1c7378e439a18b0255b30c09224
Wan Luqman Hakeem <wluqmanh@gmail.com>
2024-03-25 13:24:49 +0800

    Update 00 test.py

--- 00 test.py
+++ 00 test.py
@@ -1,10 +1,15 @@
 def two_sum(self, nums, target):
-    prevMap = {}
+    prev_map = {}
 
     for i, n in enumerate(nums):
-        
+        diff = target - n
 
+        if diff in prev_map:
+            print('prev_map[%d]: %d' % (diff, prev_map[diff]))
+            return [prev_map[diff], i]
 
+        prev_map[n] = i
+        print('prev_map: ', prev_map)
 
 nums = [11, 15, 6, 2, 7]
 target = 9
